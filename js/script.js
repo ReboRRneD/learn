4970ea7434ea2476febf9d5a83c823e822280e9b
Max <reborrnedev@gmail.com>
2024-03-29 11:32:06 +0300

    first prog

--- js/script.js
+++ js/script.js
@@ -1,11 +1,41 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+const personalMovieDB =  {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+}
 
-let arr = [1, 4, 8, 21, 22]
-let doneArr = []
-let crop = arr.length
-arr.map((a) => arr.push(a/2))
-arr.splice(0, crop)
+let movieName = prompt('Один из последних просмотренных фильмов?', '')
+let	score = prompt('На сколько оцените его?', '')
+let movieNameSecond = prompt('Один из последних просмотренных фильмов?', '')
+let	scoreSecond = prompt('На сколько оцените его?', '')
+personalMovieDB.movies[movieName] = score
+personalMovieDB.movies[movieNameSecond] = scoreSecond
 
-arr.map((a) => {if (a == Math.round(a)) {doneArr.push(a)}})
-console.log(arr.splice(0, 5))
+/* Задание на урок:
 
-//yep
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
+'use strict'
+
